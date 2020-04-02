c8ce349c353f9e87489f44cb6b627afa4489b3fe
asko pakkala <test@taml.com>
2020-04-02 22:01:53 +0300

    Updates

---
+++ precache-manifest.23bdf43eedeb074c28911bf4ea061d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc36eeceeba6b1d91071efa96079c5d",
+    "url": "/Reactfront/index.html"
+  },
+  {
+    "revision": "1d44bc94223bdf83736b",
+    "url": "/Reactfront/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "599f561f20106296f329",
+    "url": "/Reactfront/static/js/2.0354d6a9.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/Reactfront/static/js/2.0354d6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d44bc94223bdf83736b",
+    "url": "/Reactfront/static/js/main.1c3c7201.chunk.js"
+  },
+  {
+    "revision": "a074b8909ff84508de44",
+    "url": "/Reactfront/static/js/runtime-main.0103c370.js"
+  }
+]);
